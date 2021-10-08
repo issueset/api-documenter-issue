e4b2b9ecc8f9ed9c1c57f66e9b80cc2f6264764f
ocavue <ocavue@gmail.com>
2021-10-08 09:35:16 +0800

    run api-extractor

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+/**
+ * MyFunction returns `false`
+ *
+ * @public
+ *
+ * Some code example:
+ *
+ * ```ts
+ * let result = MyFunction()
+ * ```
+ */
+function MyFunction() {
+    return false
+}
+
+export {MyFunction}
